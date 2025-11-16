499c18f68ef8107fe6b52ef02bccd926b4344657
Ananthan K S <ananthanks12@gmail.com>
2025-11-16 13:43:05 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
+N = int(input("Enter the number of qubits: "))
+qr = QuantumRegister(N, 'q')
+cr = ClassicalRegister(N, 'c')
+qc = QuantumCircuit(qr, cr)
+qc.h(qr) # Applying Hadamard gate to all qubits
+qc.measure(qr, cr)
+
+from qiskit_aer import AerSimulator
+sim = AerSimulator()
+job = sim.run(qc, shots=1)
+result = job.result()
+random_string = list(result.get_counts().keys())[0]
+print(random_string)
+
